67483082c0c4c284c19555654d336b6caaa007ed
Mabb <mdutta7@gatech.edu>
2017-09-24 10:53:27 -0400

    NewComm

--- MachineLearningAnalysis/boost_train.py
+++ MachineLearningAnalysis/boost_train.py
@@ -3,6 +3,7 @@ import catboost
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
+from catboost import Pool
 import pandas as pd
 
 meeting = pd.read_csv('mergeWithRevenue.csv', encoding='latin-1')
@@ -16,16 +17,31 @@ train = meeting[msk]
 test = meeting[~msk]
 
 colNames = [col for col in meeting.columns.values if col not in ['Unnamed: 0.1', 'Unnamed: 0', 'id', 'imdb_id', 'Rating', 'Unnamed: 0_y', 'binary', 'domgross',	'intgross',	'budget_2013$','domgross_2013$','intgross_2013$','box_office','adjusted_box_office']]
-print(colNames)
+a = 0
+for col in colNames:
+    a += 1
+
 X_train, y_train = train[colNames], train['binary']
 model = catboost.CatBoostClassifier(iterations=2, learning_rate=1, depth=2, loss_function='Logloss')
-cat_features = [0, 2, 3, 8, 11, 12, 15, 17, 18, 20, 21, 22, 28, 29, 30, 31, 32, 33, 34, 38, 39, 40]
-model.fit(X_train, y_train, cat_features)
+
+cat_features = []
+i = 0
+for col in colNames:
+    if (meeting[col].dtype == 'object' and col != 'imdb_score'):
+       cat_features.append(i)
+    i += 1
+
+trainPool = Pool(X_train, y_train, cat_features)
+model.fit(trainPool)
 print(model)
 
 X_test, y_test = test[colNames], test['binary']
-y_pred = model.predict(X_test)
+print(y_test)
+testPool = Pool(X_test, y_test, cat_features)
+preds_class  = model.predict(testPool)
+print(len(preds_class))
+
 #predictions = [round(value) for value in y_pred]
 # evaluate predictions
-accuracy = accuracy_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, preds_class)
 print("Accuracy: %.2f%%" % (accuracy * 100.0))
